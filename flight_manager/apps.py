2b61e4fe8a2d88e274e3709b3387f82ee68ff706
trayneli <43825648+trayneli@users.noreply.github.com>
2025-04-16 23:54:55 +0000

    Update apps.py

--- flight_manager/apps.py
+++ flight_manager/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
 
-
-class ReviewsConfig(AppConfig):
-    name = 'reviews'
-
 class FlightManagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'flight_manager'
