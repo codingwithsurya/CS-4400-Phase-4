50c60573b90253b2e6109cc7f0bac7d7570e3743
trayneli <jtraynelis3@gatech.edu>
2025-04-30 15:35:01 -0400

    update test cases

--- TestCasesDemo.sql
+++ TestCasesDemo.sql
@@ -9,10 +9,10 @@ on ap.airlineID=al.airlineID;
 
 -- Test Cases Add airplane Sucess (Works Currently)
 call add_airplane('Delta', 'n281fc', 6, 500, 'plane_41', 'Airbus', null, null, TRUE);
+-- Note neo is integer column, so shows True=1
 
 select * from airplane where tail_num='n281fc';
 
-select neo from airplane
 
 select * from airplane ap 
 join airline al 
@@ -28,7 +28,7 @@ select * from airport where airportID='JFK';
 
 -- Test Cases Add airport Failure
 
--- Test Cases Add Person Success (error currently? )
+-- Test Cases Add Person Success
 call add_person('p61', 'Sabrina', 'Duncan', 'port_1', '366-50-3732', 27, null, null);
 
 select * from pilot;
@@ -85,6 +85,14 @@ where flightID='dl_42');
 -- Test Cases (Passenger board) Failure Case
 call passengers_board('');
 
+select * from passenger pa
+join person pe on 
+pa.personID=pe.personID where locationID in 
+(select locationID from flight fl
+join airplane a on fl.support_tail=a.tail_num
+where flightID='');
+
+
 -- Test Cases passengers_disembark Success Case
 call passengers_disembark('lf_67');
 
@@ -119,15 +127,14 @@ select * from flight fl
 join airplane ai on fl.support_tail=ai.tail_num
 where flightID='ke_64';
 
-select leg.arrival
+select *
 from route_path
 join leg on route_path.legID = leg.legID
-where route_path.routeID = @route_id 
-and route_path.sequence = @current_progress;
+where route_path.routeID = 'korea_direct' ## Fill in from query above 
+and route_path.sequence = 1; ## Fill in from query above
 
-
-select * from airport;
-
+-- Recycle pilots. shouldn't be commanding flight
+select * from pilot where commanding_flight='ke_64';
 call recycle_crew('ke_64');
 
 -- Test Cases retire_flight Success Case
@@ -136,6 +143,9 @@ call retire_flight('ke_88');
 -- TO DO: Simulation Cycle and Sequences
 
 
+-- Sample Sequence
+call passengers_board('dl_42');
+call flight_takeoff('dl_42');
 
 -- Check Views (Matching)
 select * from alternative_airports;
