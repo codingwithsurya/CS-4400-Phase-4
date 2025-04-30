af9aa4761460db6ce329e5c596e80545302d8df5
trayneli <jtraynelis3@gatech.edu>
2025-04-30 13:59:29 -0400

    clean up directory

--- TestCasesDemo.sql
+++ TestCasesDemo.sql
@@ -10,43 +10,67 @@ on ap.airlineID=al.airlineID;
 -- Test Cases Add airplane Sucess (Works Currently)
 call add_airplane('Delta', 'n281fc', 6, 500, 'plane_41', 'Airbus', null, null, TRUE);
 
+select * from airplane where tail_num='n281fc';
+
+select neo from airplane
+
 select * from airplane ap 
 join airline al 
 on ap.airlineID=al.airlineID;
 
 -- Test Cases Add airplane Failure
+select * from person;
 
 -- Test Cases Add airport Success (Works Currently)
 call add_airport('JFK', 'John F_Kennedy International', 'New York', 'New York', 'USA', 'port_33');
 
-select * from airport;
+select * from airport where airportID='JFK';
 
 -- Test Cases Add airport Failure
 
 -- Test Cases Add Person Success (error currently? )
-# Error: (1318, 'Incorrect number of arguments for PROCEDURE flight_tracking.add_airplane; expected 9, got 8')
 call add_person('p61', 'Sabrina', 'Duncan', 'port_1', '366-50-3732', 27, null, null);
+
+select * from pilot;
+
+select * from person pe
+left join passenger pa on pa.personID=pe.personID
+left join pilot pil on pil.personID=pe.personID;
 
 -- Test Cases Add Person Failure
 
 -- Test Cases grant or revoke pilot Success
 call grant_or_revoke_pilot_license('p1','jets');
 
+select * from pilot pil
+join pilot_licenses pl on pil.personID=pl.personID
+where pil.personID='p1';
+
 -- Test Cases grant or revoke pilot Failure
 
 -- Test Cases offer flight Success
 call offer_flight('un_41', 'americas_three', 'United', 'n330ss', 0, '11:30:00', 400);
+
+select * from flight 
+where flightID='un_41';
+
 
 -- Test Cases offer flight Failure
 
 -- Test Cases (flight landing) Sucess Case
 call flight_landing('dl_10');
 
+select * from flight where flightID='dl_10';
+
 -- Test Cases (flight landing) Failure Case
 call flight_landing('oh_99');
 
+select * from flight where flightID='oh_99';
+
+
 -- Test Cases (flight takeoff) Success Case
 call flight_takeoff('ba_61');
+select * from flight where flightID='ba_61';
 
 -- Test Cases (flight takeoff) Failure Case
 call flight_takeoff('oh_99');
@@ -66,6 +90,11 @@ call passengers_disembark('lf_20');
 -- Test Cases assign_pilot Success Case
 call assign_pilot('ry_34', 'p19');
 
+-- Before being assigned, commanding flight is null;
+select * from pilot pil
+join person pe on pil.personID=pe.personID
+where pe.personID='p19';
+
 -- Test Cases recycle_crew Success Case
 call recycle_crew('ke_64');
 
@@ -73,5 +102,18 @@ call recycle_crew('ke_64');
 call retire_flight('ke_88');
 
 -- TO DO: Simulation Cycle and Sequences
+
+-- Check Views
+select * from alternative_airports;
+select * from flights_in_the_air;
+select * from flights_on_the_ground;
+select * from people_in_the_air;
+select * from people_on_the_ground;
+select * from route_summary;
+
+
+
+
+
 
 
